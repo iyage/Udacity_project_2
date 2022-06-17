5bf1b5a058aa92ee1d8d5d6dc3e19162877da27f
Ofunrein Iyaghe Godwin <yahg.concept@gmail.com>
2022-06-17 10:58:58 +0100

    minor changes

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation update-stack --stack-name udacityProject2 --template-body file://$1  --parameters file://$2 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+aws cloudformation update-stack --stack-name udacityProject2 --template-body file://network.yaml --parameters file://parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
--- updateServer.sh
+++ updateServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation update-stack --stack-name udacityProject2server --template-body file://server.yaml  --parameters file://serverParameter.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+aws cloudformation update-stack --stack-name projectServer --template-body file://server.yaml  --parameters file://serverParameter.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
